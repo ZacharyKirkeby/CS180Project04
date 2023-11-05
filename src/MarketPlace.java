69479b8852d21894e2034e7b323a63f488133a76
Zach <zakirkeby@gmail.com>
2023-11-05 17:07:28 -0500

    "Updated MarketPlace"

--- src/MarketPlace.java
+++ src/MarketPlace.java
@@ -6,11 +6,15 @@ import java.util.Scanner;
 public class MarketPlace {
     private static final String WELCOMEPROMPT = "Login or Register an Account (Login / Register)";
     private static final String sellerChoices = "1. Create Store \n 2. Modify Store " +
-            "\n 3. View Store Statistics \n 4. Delete Store ";
+            "\n 3. View Store Statistics \n 4. Delete Store \n 5. Manage Account \n";
+    private static final String BUYERPROMPT = "1. Search for a store \n 2. Search for a product \n" +
+            "3. Search by Description \n 4. View All Products \n 6. Manage Account \n";
+    private static final String SEARCHPROMPT = "Enter search term: ";
     private ArrayList<Store> stores;
+    private static boolean isLoggedIn;
 
     public MarketPlace() {
-
+        // some kind of logic tbd
     }
 
 
@@ -25,29 +29,53 @@ public class MarketPlace {
                 System.out.println("Enter your password: ");
                 String password = scanner.nextLine();
                 Account.login(user, password);
+                isLoggedIn = true;
+                while (isLoggedIn) {
 
-                if (Account.getRole(user).equals("seller")) {
-                    System.out.print(sellerChoices);
-                    input = scanner.nextLine();
-                    switch (input){
-                        case "1":
-                            System.out.println("Enter a store name: ");
-                            String storeName = scanner.nextLine();
-                            System.out.println("Enter a store location: ");
-                            String location = scanner.nextLine();
-                            Seller.createStore(storeName, location, user);
-                            break;
-                        case "2":
+                    if (Account.getRole(user).equalsIgnoreCase("seller")) {
+                        System.out.print(sellerChoices);
+                        input = scanner.nextLine();
+                        switch (input) {
+                            case "1":
+                                System.out.println("Enter a store name: ");
+                                String storeName = scanner.nextLine();
+                                System.out.println("Enter a store location: ");
+                                String location = scanner.nextLine();
+                                Seller.createStore(storeName, location, user);
+                                break;
+                            case "2":
 
-                            break;
-                        case "3":
-                            break;
+                                break;
+                            case "3":
+                                break;
+                        }
+
+                    } else if (Account.getRole(user).equals("customer")) {
+                        System.out.println(BUYERPROMPT);
+                        input = scanner.nextLine();
+                        switch (input){
+                            case "1":
+                                System.out.println(SEARCHPROMPT);
+                                input = scanner.nextLine();
+                                //search logic
+                                break;
+                            case "2":
+                                System.out.println(SEARCHPROMPT);
+                                input = scanner.nextLine();
+                                //search logic
+                                break;
+                            case "3":
+                                System.out.println(SEARCHPROMPT);
+                                input = scanner.nextLine();
+                                //search logic
+                                break;
+                            case "4":
+                                Seller.printProductAndStores();
+                        }
+
+
                     }
-
-                } else if (Account.getRole(user).equals("customer")) {
-
                 }
-
                 break;
             case "Register":
                 System.out.println("Enter an email: ");
