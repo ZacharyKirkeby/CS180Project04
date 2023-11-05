2d6bfa288a264e51b5304ffdb5895533ed44301a
Zach <zakirkeby@gmail.com>
2023-11-05 15:10:14 -0500

    "latest store"

--- src/Store.java
+++ src/Store.java
@@ -15,23 +15,67 @@ public class Store {
     private String storeName;
     private String storeLocation;
     private Seller storeOwner;
+    private String sellserUsername;
 
     public Store(String name, String storeLocation, Seller seller,
-                  ArrayList<Product> productList) {
+                 ArrayList<Product> productList) {
         this.storeName = name;
         this.storeLocation = storeLocation;
         this.storeOwner = seller;
         this.productList = productList;
     }
+    public Store(String name, String storeLocation, String sellserUsername,
+                 ArrayList<Product> productList) {
+        this.storeName = name;
+        this.storeLocation = storeLocation;
+        this.sellserUsername = sellserUsername;
+        this.productList = productList;
+    }
+
+    public ArrayList<Product> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(ArrayList<Product> productList) {
+        this.productList = productList;
+    }
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    public String getStoreLocation() {
+        return storeLocation;
+    }
+
+    public void setStoreLocation(String storeLocation) {
+        this.storeLocation = storeLocation;
+    }
+
+    public Seller getStoreOwner() {
+        return storeOwner;
+    }
+
+    public void setStoreOwner(Seller storeOwner) {
+        this.storeOwner = storeOwner;
+    }
 
     public Store() {
         this.storeName = "";
         this.storeLocation = "";
-        this.storeOwner = new Seller();
+        this.storeOwner = new Seller("", "", "");
         this.productList = new ArrayList<Product>();
     }
 
+    public String getSellserUsername() {
+        return sellserUsername;
+    }
 
-
-
+    public void setSellserUsername(String sellserUsername) {
+        this.sellserUsername = sellserUsername;
+    }
 }
