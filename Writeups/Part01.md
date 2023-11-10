2c7e0303d60bcfa9375a30b1f11e64f1b96e6ae4
Zach <zakirkeby@gmail.com>
2023-11-10 13:11:36 -0500

    "Fixed store methods / seller"

--- Writeups/Part01.md
+++ Writeups/Part01.md
@@ -1,7 +1,7 @@
 # Part 1
 
 ## Project Overview
-
+***
 Our group selected Option 3 for our specific implementation of Project 4. Since our group has 5 members, we included all
 three selection options. 
 
@@ -9,19 +9,20 @@ From a high level, our project is composed of 6 classes, which implement all cor
 requirements. 
 
 ### Account
-
+***
 
 
 ### Customer
+***
 
 ### MarketPlace
-
+***
 Overarching handler for Sellers, Store, and Customer classes. Creates a looping menu with options depending on user
 designation. First prompts the user to login or register a new account. From there, a looping menu is created showing 
 options based on user designation. 
 
 #### Seller Options
-
+***
 If the user is designated as a seller, options are presented to create a new store, manage an existing store, view
 statistics, delete an existing store, modify their account, or logout. Modify account and logout are not unique to 
 seller designated accounts. 
@@ -35,14 +36,14 @@ The statistics menu presents sorting options, customers and purchase records, sh
 export product listings to a file. 
 
 #### Customer Options
-
+***
 The customer designated users are prompted with search options. They can search by store specifically, by product, or
 just return everything. 
 
 Lastly, the customer can purchase items, add to a shopping cart, view their shopping cart ... 
 
 #### Account Management (General Feature)
-
+***
 Users, regardless of designation can change their username, email, password and can also logout. 
 
 ### Product
--- src/Store.java
+++ src/Store.java
@@ -1,12 +1,13 @@
 package src;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Collections;
 
 /**
  * Project 04 -- Store.java
  * creates Store class
- * WIP
+ * Handles routine store tasks
  *
  * @author Zachary Kirkeby, 05
  * @version November 3, 2023
@@ -20,6 +21,7 @@ public class Store {
     private int totalSales = 0;
     private double totalRevenue = 0;
 
+    // Constructor for store based off a literal seller object
     public Store(String name, String storeLocation, Seller seller,
                  ArrayList<Product> productList) {
         this.storeName = name;
@@ -27,7 +29,7 @@ public class Store {
         this.storeOwner = seller;
         this.productList = productList;
     }
-
+    // More generic Store constructor based off a named seller
     public Store(String name, String storeLocation, String sellerUsername,
                  ArrayList<Product> productList) {
         this.storeName = name;
@@ -35,14 +37,14 @@ public class Store {
         this.sellerUsername = sellerUsername;
         this.productList = productList;
     }
-
+    // gets total number of products sold
     public int getTotalSales() {
         for(Product p:productList) {
             totalSales += p.getQuantitySold();
         }
         return totalSales;
     }
-
+    // calculates total revenue by getting sales * price
     public double getTotalRevenue() {
         for(Product p:productList) {
             totalRevenue += p.getSales();
@@ -50,27 +52,27 @@ public class Store {
         return totalRevenue;
     }
 
-
+    // returns arraylist of products
     public ArrayList<Product> getProductList() {
         return productList;
     }
-
+    // generic setter
     public void setProductList(ArrayList<Product> productList) {
         this.productList = productList;
     }
-
+    // returns store name
     public String getStoreName() {
         return storeName;
     }
-
+    // allows for renaming store
     public void setStoreName(String storeName) {
         this.storeName = storeName;
     }
-
+    // generic getter
     public String getStoreLocation() {
         return storeLocation;
     }
-
+    // generic setter
     public void setStoreLocation(String storeLocation) {
         this.storeLocation = storeLocation;
     }
@@ -103,18 +105,40 @@ public class Store {
         this.sellerUsername = sellserUsername;
     }
 
-    public static String getSortedCustomersAndPurchases (ArrayList < Store > stores) {
-        for (int i = 0; i < stores.size(); i++) {
-            for (int j = i + 1; j < stores.size(); j++) {
-                if (stores.get(i).getStoreName().compareTo(stores.get(j).getStoreName()) > 0) {
-                    Collections.swap(stores, i, j);
+    /**
+     * Iterates through purchase history file
+     * if store name corosponds to this store name
+     * adds line
+     * returns end list of customers and their purchases
+     **/
+    public  String getSortedCustomersAndPurchases () {
+        String sentence = "";
+        try (BufferedReader reader = new BufferedReader(new FileReader("PurchaseHistoryDatabase.txt"))) {
+            String line = reader.readLine();
+            while (line != null) {
+                String[] subpart = line.split(",");
+                if (subpart[3].equals(storeName)) {
+                    sentence += line;
                 }
+                line = reader.readLine();
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return stores.toString();
+        return sentence;
     }
-    public static String getCustomersAndPurchases (ArrayList < Store > stores) {
-        return stores.toString();
+    public static String getCustomersAndPurchases () {
+        String sentence = "";
+        try (BufferedReader reader = new BufferedReader(new FileReader("PurchaseHistoryDatabase.txt"))) {
+            String line = reader.readLine();
+            while (line != null) {
+                sentence += line;
+                line = reader.readLine();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sentence;
     }
     public void Purchases () {
         double totalPurchases = 0;
