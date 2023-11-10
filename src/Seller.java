cf70dea2477c62861008d4f7b0ba2d45d141cac8
Armaan Sayyad <83065289+ArmaanSayyad@users.noreply.github.com>
2023-11-10 18:49:01 -0500

    Update Seller.java Added seller review viewing algorithm

--- src/Seller.java
+++ src/Seller.java
@@ -1,5 +1,4 @@
 package src;
-
 import java.io.*;
 import java.util.*;
 
@@ -796,7 +795,26 @@ public abstract class Seller {
         result = result.replace(";", " | ");
         return result;
     }
-
+    public static String viewCustomerReviews(String productName, String user) {
+        String result = "";
+        if(!(productName.equals(""))) {
+            for (int i = 0; i < stores.size(); i++) {
+                if (stores.get(i).getSellerUsername().equalsIgnoreCase(user)) {
+                    result += Customer.viewReviews(stores.get(i).getStoreName(), productName) + "\n";
+                }
+            }
+        } else if(productName.equals("")){
+            for(int i = 0; i < stores.size(); i++){
+                if(stores.get(i).getSellerUsername().equalsIgnoreCase(user)) {
+                    for (int j = 0; j < stores.get(i).getProductList().size(); i++) {
+                        result += Customer.viewReviews(stores.get(i).getStoreName(),
+                                stores.get(i).getProductList().get(j).getName() + "\n");
+                    }
+                }
+            }
+        }
+        return result;
+    }
 
 
 }
